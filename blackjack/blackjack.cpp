6634b03981c3a5bd0f155521461265b6a60d8da3
kino24 <joaquinsantos409@gmail.com>
2021-07-21 23:16:05 +0800

    Updated enemy bot

--- blackjack/blackjack.cpp
+++ blackjack/blackjack.cpp
@@ -8,11 +8,11 @@ int main()
     srand(time(0));
     int player=0,blackJackDealer=0,initialBotValue=0,cards=1;
     string choice;
-    cout<<" type 'hit' to draw another card, and type 'stay' to attack"<<endl;
+    cout<<" type 'hit' to draw another card, and type 'stay' to pass"<<endl;
     for(int i=0; i<2; i++)
     {
-        player += rand()%10+1;
-        blackJackDealer += rand()%10+1;
+        player += rand()%11+1;
+        blackJackDealer += rand()%11+1;
         if(i==0)
             initialBotValue=blackJackDealer;
     }
@@ -25,9 +25,9 @@ int main()
         if(choice=="hit")
         {
             cards++;
-            player +=rand()%10+1;
-            if(rand()%2+1==2||blackJackDealer<15)
-                blackJackDealer+=rand()%10+1;
+            player +=rand()%11+1;
+            if(rand()%2+1==2||blackJackDealer<17)
+                blackJackDealer+=rand()%11+1;
             if(player>=21)
                 break;
             cout<<"Player: "<<player<<endl
@@ -56,5 +56,6 @@ int main()
     else if(player==blackJackDealer)
         cout<<"It is a tie!"<<endl;
     
+    system("pause");
     return 0;
 }
